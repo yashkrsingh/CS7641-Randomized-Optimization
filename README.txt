a647d3c03b7dfb9892e7d99c71b2873c208ed98f
Singh, Yash Kumar <yash_kumar_singh@dell.com>
2022-03-05 13:44:56 +0530

    Added Code for Part 1 and Partial Part 2

--- README.txt
+++ README.txt
@@ -12,6 +12,8 @@ Project Structure
 1. data
     - fetal-health.csv: Dataset taken from Kaggle with 10+ attributes of fetus health during pregnancy with their subsequent class labels.
     - fetal-health.names: Feature headers for breast cancer dataset
+    - winequality-white.csv: Dataset taken from UCI ML Repository, containing 11 attributes of different white wines and their quality label as judges by experts on the scale of 1-10
+    - winequality.names: Feature headers for winequality dataset
 
 2. scripts
     - processing.py: Contains functions for data preprocessing and result consolidation, including validation and learning curve creation.
--- scripts/processing.py
+++ scripts/processing.py
@@ -1,21 +1,25 @@
+from copy import deepcopy
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import mlrose_hiive as mlrose
+import time
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import learning_curve
-from sklearn.model_selection import validation_curve
 
 
-def load_cancer_data():
-    cancer = pd.read_csv('../data/breast-cancer-wisconsin.csv', sep=',', header=0)
-    cancer.drop(columns=['Sample code number'], inplace=True)
-    cancer['Class'] = np.where(cancer['Class'] == 4, 1, 0)
-    cancer = cancer[cancer['Bare Nuclei'] != '?']
-    # print(cancer['Class'].value_counts())
-    return cancer
-
+def load_fetus_data():
+    fetus = pd.read_csv('../data/fetal-health.csv', sep=',', header=0)
+    col_to_drop = ['histogram_width', 'histogram_min', 'histogram_max', 'histogram_number_of_peaks',
+                   'histogram_number_of_zeroes', 'histogram_mode', 'histogram_mean', 'histogram_median',
+                   'histogram_variance', 'histogram_tendency']
+    fetus = fetus.drop(col_to_drop, axis=1)
+    fetus["fetal_health"].replace({1: 0, 2: 1, 3: 1}, inplace=True)
+    # print(fetus['fetal_health'].value_counts())
+    return fetus
 
 def load_wine_data():
     wine = pd.read_csv('../data/winequality-white.csv', sep=',', header=0)
@@ -24,15 +28,6 @@ def load_wine_data():
     wine['quality'] = pd.cut(wine['quality'], bins=bins, labels=labels)
     # print(wine['quality'].value_counts())
     return wine
-
-
-def load_fetus_data():
-    fetus = pd.read_csv('../data/fetal-health.csv', sep=',', header=0)
-    col_to_drop = ['histogram_width', 'histogram_min', 'histogram_max', 'histogram_number_of_peaks', 'histogram_number_of_zeroes', 'histogram_mode', 'histogram_mean', 'histogram_median', 'histogram_variance', 'histogram_tendency']
-    fetus = fetus.drop(col_to_drop, axis=1)
-    fetus["fetal_health"].replace({1: 0, 2: 1, 3: 1}, inplace=True)
-    # print(fetus['fetal_health'].value_counts())
-    return fetus
 
 
 def split_data_set(dataframe, seed):
@@ -87,55 +82,6 @@ def plot_learning_curve(data_name, estimator, train_x, train_y, score_metric):
     axes[1].set_title("Scalability of the model")
 
     name = 'learning curve'
-    plt.savefig(f'{data_name}_{estimator.__class__.__name__}_{name}.png',
-                dpi=200, bbox_inches='tight')
-
-
-def plot_validation_curve(data_name, estimator, parameters, train_x, train_y, score_metric):
-    np.random.seed(42)
-    plt.clf()
-    n = len(parameters)
-    fig, axes = plt.subplots(1, n, figsize=(20, 3))
-    for i, param_name in enumerate(parameters):
-        param_range = parameters[param_name]
-        train_scores, test_scores = validation_curve(estimator, train_x, train_y,
-                                                     param_name=param_name, param_range=param_range, n_jobs=-1,
-                                                     scoring=score_metric)
-
-        train_scores_mean = np.mean(train_scores, axis=1)
-        train_scores_std = np.std(train_scores, axis=1)
-        test_scores_mean = np.mean(test_scores, axis=1)
-        test_scores_std = np.std(test_scores, axis=1)
-
-        if all([type(p) is tuple for p in param_range]):
-            param_range = list(map(str, param_range))
-
-        if all([type(p) is str for p in param_range]):
-            print(f"Mapping {param_range} for {param_name}")
-            param_labels = param_range
-            param_range = list(range(len(param_range)))
-        else:
-            param_labels = None
-
-        if n > 1:
-            ax = axes[i]
-        else:
-            ax = axes
-        ax.grid()
-        ax.set_xticks(param_range)
-        if param_labels is not None:
-            ax.set_xticklabels(labels=param_labels)
-        ax.set_xlabel(param_name)
-        ax.set_ylabel("Score")
-        lw = 2
-
-        ax.plot(param_range, train_scores_mean, label="Training score", color="red", lw=lw)
-        ax.fill_between(param_range, train_scores_mean - train_scores_std, train_scores_mean + train_scores_std, alpha=0.2, color="#DDDDDD", lw=lw)
-        ax.plot(param_range, test_scores_mean, label="Cross-validation score", color="navy", lw=lw)
-        ax.fill_between(param_range, test_scores_mean - test_scores_std, test_scores_mean + test_scores_std, alpha=0.2, color="#DDDDDD", lw=lw)
-        ax.legend(loc="best")
-
-    name = 'validation curve'
     plt.savefig(f'{data_name}_{estimator.__class__.__name__}_{name}.png', dpi=200, bbox_inches='tight')
 
 
@@ -147,3 +93,81 @@ def classification_scores(data, classification_report):
 
     return [data, precision, recall, f1, accuracy]
 
+
+def run_randomized_optimizer(name, problem, results, seed):
+    curves = {}
+    fit = []
+
+    start = time.perf_counter()
+    best_state, best_fitness, curve = mlrose.simulated_annealing(problem, curve=True, random_state=seed, max_attempts=10)
+    tt = time.perf_counter() - start
+    results.loc[results.shape[0]] = [name, 'sa', best_fitness, len(curve), problem.fitness_evaluations, tt]
+    curve = deepcopy(curve)
+    curves['sa'] = curve
+    fit.append(best_fitness)
+
+    start = time.perf_counter()
+    best_state, best_fitness, curve = mlrose.random_hill_climb(problem, curve=True, random_state=seed, max_attempts=10)
+    tt = time.perf_counter() - start
+    results.loc[results.shape[0]] = [name, 'rhc', best_fitness, len(curve), problem.fitness_evaluations, tt]
+    curve = deepcopy(curve)
+    curves['rhc'] = curve
+    fit.append(best_fitness)
+
+    start = time.perf_counter()
+    best_state, best_fitness, curve = mlrose.genetic_alg(problem, curve=True, random_state=seed, max_attempts=10)
+    tt = time.perf_counter() - start
+    results.loc[results.shape[0]] = [name, 'ga', best_fitness, len(curve), problem.fitness_evaluations, tt]
+    curve = deepcopy(curve)
+    curves['ga'] = curve
+    fit.append(best_fitness)
+
+    start = time.perf_counter()
+    best_state, best_fitness, curve = mlrose.mimic(problem, curve=True, random_state=seed, max_attempts=10)
+    tt = time.perf_counter() - start
+    results.loc[results.shape[0]] = [name, 'mimic', best_fitness, len(curve), problem.fitness_evaluations, tt]
+    curve = deepcopy(curve)
+    curves['mimic'] = curve
+    fit.append(best_fitness)
+
+    return results, curves, max(fit)
+
+
+def normalize(arr, max=None):
+    arr = np.asarray(arr)
+    if max is None:
+        arr -= arr.min()
+        arr /= arr.max()
+    else:
+        arr /= max
+    return arr
+
+
+def evaluate_random_algorithms(name, curve_dict, max, is_nn=False):
+    plt.clf()
+    fig, axes = plt.subplots(1, 2, figsize=(20, 5))
+    ylabel = 'Loss' if is_nn else 'Fitness'
+    axes[0].set_xlabel("Iterations%")
+    axes[0].set_ylabel(f"{ylabel}%")
+    axes[0].grid()
+
+    axes[1].set_xlabel("Fevals%")
+    axes[1].set_ylabel(f"{ylabel}%")
+    axes[1].grid()
+
+    for alg, curve in curve_dict.items():
+        fitness_by_iterations = normalize([c[0] for c in curve], max)
+        fevals_by_iterations = normalize([c[1] for c in curve])
+
+        iterations = normalize(list(map(float, range(len(curve)))))
+
+        axes[0].plot(iterations, fitness_by_iterations, label=alg)
+        axes[1].plot(fevals_by_iterations, fitness_by_iterations, label=alg)
+
+    axes[0].legend(loc="best")
+    axes[1].legend(loc="best")
+    title_name = name
+    if is_nn:
+        title_name = "Neural Net Optimization"
+    fig.suptitle(f'Comparison of Normalized Characteristics for {title_name}')
+    plt.savefig(f'Compare_{name}.png', dpi=200, bbox_inches='tight')
